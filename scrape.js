204c68eb0d98695a48fdc4c2da15b6d667bb7d1b
Kassandra Perch <the@nodebotani.st>
2018-12-30 12:03:23 -0600

    Writes a list of pokemon to a JSON file (yes, it catches them all)

--- scrape.js
+++ scrape.js
@@ -1,9 +1,34 @@
+const fs = require('fs')
+const path = require('path')
+
 const request = require('request')
 const cheerio = require('cheerio')
 
 request.get('https://pokemondb.net/pokedex/game/lets-go-pikachu-eevee', (err, response, body) => {
+  if (err) {
+    throw new Error(err)
+  }
+  let result = {}
   const $ = cheerio.load(body)
-  $('.infocard .ent-name').each(function (number, pokemon) {
-    console.log(`#${number + 1} -- ${$(this).text()}`)
+  let imgurl, name, types
+  $('.infocard').each(function (number) {
+    imgurl = $(this).find('.img-sprite').attr('src')
+    name = $(this).find('.ent-name').text()
+    types = []
+    $(this).find('a.itype').each(function () {
+      types.push($(this).text())
+    })
+    console.log('image url:', imgurl, '\nname: ', name)
+    result[number + 1] = {
+      imgurl,
+      name,
+      types
+    }
+  })
+  fs.writeFile(path.resolve(__dirname, './data/pokemon.json'), JSON.stringify(result), (err) => {
+    if (err) {
+      throw new Error(err)
+    }
+    console.log('wrote the pokedex file!')
   })
 })
