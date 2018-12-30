89e278d0a904978f45215114eb4db673367c4bbc
Kassandra Perch <the@nodebotani.st>
2018-12-30 12:29:53 -0600

    yay it downloads the sprites!

--- pokemon-scraper.js
+++ pokemon-scraper.js
@@ -4,23 +4,33 @@ const path = require('path')
 const request = require('request')
 const cheerio = require('cheerio')
 
+const download = function (uri, filename, callback) {
+  request.head(uri, function (err, res, body) {
+    if (err) {
+      throw new Error(err)
+    }
+    request(uri).pipe(fs.createWriteStream(path.resolve(__dirname, filename))).on('close', callback)
+  })
+}
+
 request.get('https://pokemondb.net/pokedex/game/lets-go-pikachu-eevee', (err, response, body) => {
   if (err) {
     throw new Error(err)
   }
   let result = {}
   const $ = cheerio.load(body)
-  let imgurl, name, types
+  let imgPath, name, types
   $('.infocard').each(function (number) {
-    imgurl = $(this).find('.img-sprite').attr('src')
     name = $(this).find('.ent-name').text()
+    imgPath = './data/img/' + name + '.png'
+    download($(this).find('.img-sprite').attr('src'), imgPath, (err) => { if (err) throw new Error(err) })
     types = []
     $(this).find('a.itype').each(function () {
       types.push($(this).text())
     })
-    console.log('image url:', imgurl, '\nname: ', name)
+    console.log('image path:', imgPath, '\nname: ', name)
     result[number + 1] = {
-      imgurl,
+      imgPath,
       name,
       types
     }
